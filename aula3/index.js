93aca4f0f654427dc75f388c8ff81ba88af37016
mandrado <mandrado@mandrado.com>
2021-06-19 17:20:15 -0300

    9. Execícios e aula sobre console.log()

---
+++ aula3/index.js
@@ -0,0 +1,12 @@
+const { date } = require("joi");
+
+console.log('Olá Mundo!'); // string = texto
+console.log('Olá "Mundo!"');
+console.log("Olá 'Mundo!'");
+console.log(`'"Olá" 'Mundo!''`);
+
+console.log(1+2); // number = número (não tem ponto flutuante ou real)
+console.log(15.53);
+console.log(15, 1.5, 'Mandrado Silva dos Santos');
+
+console.log('Meu nome é "Mandrado". Estou aprendendo JavaScript às', 10,'da manhã');
