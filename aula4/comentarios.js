1cbba6c4404f1c53b824f84d02c8f9382d57ca4c
mandrado <mandrado@mandrado.com>
2021-06-21 19:10:49 -0300

    12. Comentários de código

---
+++ aula4/comentarios.js
@@ -0,0 +1,19 @@
+// escrever um comentário de linha
+console.log('Olá mundo!'); // aqui tem outro comentário
+console.log('Linha 1');
+//console.log('Linha 2');
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+// vai ser ignorado
+
+/* comentário de bloco
+onde podemos ter várias linhas de texto
+com informações sobre o código */
+
+/**
+ * agora o texto de comnentário longo
+ * está decorado, rsrs
+ * 
+ */
