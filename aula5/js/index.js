6d9d1d833d5a704c40341142bb04f211f58a3e57
mandrado <mandrado@mandrado.com>
2021-06-21 19:43:05 -0300

    13. Navegador vs Node (HTML + JavaScript)

---
+++ aula5/js/index.js
@@ -0,0 +1,5 @@
+console.log('oi')
+// um comentário normal
+console.log('Olá Mundo!');
+console.log('Este trecho será exibido no console do navegador');
+alert('este é um alerta para vc ver no navegador html, mas não no terminal');
