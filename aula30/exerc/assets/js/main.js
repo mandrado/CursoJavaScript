e5538c27866b7981df90c238ad696c5beca9e20d
mandrado <mandrado@mandrado.com>
2021-06-30 20:23:05 -0300

    48. Exercício com switch e date (minha versão)

---
+++ aula30/exerc/assets/js/main.js
@@ -0,0 +1,108 @@
+function diaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            break;
+        case 1:
+            diaSemanaTexto = 'Segunda-feira';
+            break;
+        case 2:
+            diaSemanaTexto = 'Terça-feira';
+            break;
+        case 3:
+            diaSemanaTexto = 'Quarta-feira';
+            break;
+        case 4:
+            diaSemanaTexto = 'Quinta-feira';
+            break;
+        case 5:
+            diaSemanaTexto = 'Sexta-feira';
+            break;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            break;
+    
+        default:
+            diaSemanaTexto = 'Dia da semana incorreto';
+            break;
+    }
+
+    return diaSemanaTexto;
+    
+}
+
+function mesTexto(mes) {
+    let mestxt;
+    switch (mes) {
+        case 0:
+            mestxt = 'janeiro'
+            break;
+        case 1:
+            mestxt = 'fevereiro'
+            break;
+        case 2:
+            mestxt = 'março'
+            break;
+        case 3:
+            mestxt = 'abril'
+            break;
+        case 4:
+            mestxt = 'maio'
+            break;
+        case 5:
+            mestxt = 'junho'
+            break;
+        case 6:
+            mestxt = 'julho'
+            break;
+        case 7:
+            mestxt = 'agosto'
+            break;
+        case 8:
+            mestxt = 'setembro'
+            break;
+        case 9:
+            mestxt = 'outubro'
+            break;
+        case 10:
+            mestxt = 'novembro'
+            break;
+        case 11:
+            mestxt = 'dezembro'
+            break;
+    
+        default:
+            mestxt = 'mes incorreto.'
+            break;
+    }
+    return mestxt;
+}
+
+function zeroEsquerda(num) {
+    return num >=10 ? num : `0${num}`;
+}
+
+function formatHora(hora) {
+    const hs = zeroEsquerda(hora.getHours());
+    const min = zeroEsquerda(hora.getMinutes());
+    return `${hs}:${min}`
+}
+
+
+const dataAtual = new Date(Date.now());
+//const dataAtual = new Date('2019-5-5 1:3:15');
+console.log(dataAtual.getMonth());
+
+const diaSemanaTxt = diaSemanaTexto(dataAtual.getDay());
+const mes = mesTexto(dataAtual.getMonth());
+
+const hora = formatHora(dataAtual);
+
+const msg = `${diaSemanaTxt}, ${dataAtual.getDate()} de ${mes} de ${dataAtual.getFullYear()} ${hora}`;
+
+const myHtml = document.querySelector('#data');
+myHtml.innerHTML = msg;
+
+console.log(`${diaSemanaTxt}, ${dataAtual.getDate()} de ${mes} de ${dataAtual.getFullYear()} ${hora}`);
+
