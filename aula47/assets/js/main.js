ca5fb2cc95eda89ad97fb2405341fdfede90c04d
mandrado <mandrado@mandrado.com>
2021-07-08 15:04:07 -0300

    66. Exercício - Criando um timer setInterval (v3)

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -1,47 +1,50 @@
 //66. Exercício - Criando um timer com setInterval
 
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-let segundos = 0;
-let timer;
+function meuTimer() {
 
-function getDataSegundos(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+
+    function getDataSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    function iniciarTimer() {
+        timer = setInterval(() => {
+            segundos++;
+            relogio.innerHTML = getDataSegundos(segundos);
+        }, 1000);
+    }
+
+    document.addEventListener('click', function (e) {
+        const el = e.target;
+
+        if (el.classList.contains('iniciar')) {
+            clearInterval(timer);
+            iniciarTimer();
+            relogio.classList.remove('pausa');
+        }
+        if (el.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausa');
+        }
+        if (el.classList.contains('zerar')) {
+            clearInterval(timer);
+            segundos = 0;
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausa')
+        }
+
     });
+
 }
 
-function iniciarTimer() {
-    timer = setInterval(() => {
-        segundos++;
-        relogio.innerHTML = getDataSegundos(segundos);
-    }, 1000);
-}
-
-document.addEventListener('click', function (e) {
-    const el = e.target;
-
-    if (el.classList.contains('iniciar')) {
-        clearInterval(timer);
-        iniciarTimer();
-        relogio.classList.remove('pausa');
-    }
-    if (el.classList.contains('pausar')) {
-        clearInterval(timer);
-        relogio.classList.add('pausa');
-    }
-    if (el.classList.contains('zerar')) {
-        clearInterval(timer);
-        segundos = 0;
-        relogio.innerHTML = '00:00:00';
-        relogio.classList.remove('pausa')
-    }
-    
-});
+meuTimer();
 
 /* iniciar.addEventListener('click', function (event) {
     clearInterval(timer);
