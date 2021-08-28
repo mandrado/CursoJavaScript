e74dc08a3023cf0fe634595dc072247931182255
mandrado <mandrado@mandrado.com>
2021-08-28 18:14:38 -0300

    113. Requisições AJAX com Axios (JSON)

--- aula91/assets/js/main.js
+++ aula91/assets/js/main.js
@@ -1,24 +1,33 @@
-fetch('pessoas.json')
-    .then(resposta => resposta.json())
-    .then(json => carregaLementosNaPagina(json));
+/**
+ * 113. Fetch API e Axios (JSON)
+ * 
+ * Para usar o Axios, precisamos apontar no html o CDN
+ * 
+ */
+// fetch('pessoas.json')
+//     .then(resposta => resposta.json())
+//     .then(json => carregaLementosNaPagina(json));
+
+axios('pessoas.json')
+    .then(resposta => carregaLementosNaPagina(resposta.data));
 
 function carregaLementosNaPagina(json) {
     // criar uma table html
     const table = document.createElement('table');
     const tr0 = document.createElement('tr');
     let th = document.createElement('th');
-    
+
     th.innerHTML = 'Nome';
     tr0.appendChild(th);
-    
+
     th = document.createElement('th');
     th.innerHTML = 'Idade';
     tr0.appendChild(th);
-    
+
     th = document.createElement('th');
     th.innerHTML = 'Salário';
     tr0.appendChild(th);
-    
+
     table.appendChild(tr0);
 
     for (const pessoa of json) {
