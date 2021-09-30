95e9de7c2e608c7e8848c95ece15adfc3c131227
mandrado <mandrado@mandrado.com>
2021-09-30 16:22:20 -0300

    184. Tratando os arquivos enviados (PNG ou JPEG)

--- api_rest/src/controllers/ImagensController.js
+++ api_rest/src/controllers/ImagensController.js
@@ -1,7 +1,20 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('imagem');
+
 class ImagensController {
   async store(req, res) {
     // exibir dados do arquivo no retorno do upload
-    res.json(req.file);
+    // res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+      return res.json(req.file);
+    });
   }
 }
 
--- api_rest/src/routes/imagensRoute.js
+++ api_rest/src/routes/imagensRoute.js
@@ -1,15 +1,12 @@
 import { Router } from 'express';
-import multer from 'multer';
 
-import multerConfig from '../config/multer';
 import imagensController from '../controllers/ImagensController';
 
-// passar as configurações do Multer para a variável de upload
-const upload = multer(multerConfig);
+// configurações do Multer transferidas para ImagensController.
 
 const router = new Router();
 
 // criando a rota para imagens:
-router.post('/', upload.single('imagem'), imagensController.store);
+router.post('/', imagensController.store);
 
 export default router;
