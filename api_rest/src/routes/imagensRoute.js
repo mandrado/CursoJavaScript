a704964fbadd33a2a2943cadf04167b94633f52a
mandrado <mandrado@mandrado.com>
2021-09-30 16:34:25 -0300

    185. Requerendo login para salvar imagens

--- api_rest/src/routes/imagensRoute.js
+++ api_rest/src/routes/imagensRoute.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
 import imagensController from '../controllers/ImagensController';
+import loginRequired from '../middlewares/loginRequired';
 
 // configurações do Multer transferidas para ImagensController.
 
 const router = new Router();
 
-// criando a rota para imagens:
-router.post('/', imagensController.store);
+// criando a rota para imagens (requerendo login para enviar imagens):
+router.post('/', loginRequired, imagensController.store);
 
 export default router;
