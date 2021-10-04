99ba8a302ff70e346de3e8eced5fa04fb5de909d
mandrado <mandrado@mandrado.com>
2021-10-03 21:07:34 -0300

    188. Deploy da API corrigindo o endereço de upload

--- api_rest/src/app.js
+++ api_rest/src/app.js
@@ -34,7 +34,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'imagens')));
+    this.app.use('/imagens/', express.static(resolve(__dirname, '..', 'uploads', 'imagens')));
   }
 
   routes() {
--- api_rest/src/config/appConfig.js
+++ api_rest/src/config/appConfig.js
@@ -1,4 +1,5 @@
 export default
 {
   url: process.env.APP_URL,
+  port: process.env.APP_PORT,
 };
