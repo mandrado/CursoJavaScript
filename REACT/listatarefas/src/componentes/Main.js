0dcca20242051796e816b650535a852280c16395
mandrado <mandrado@mandrado.com>
2021-10-05 09:47:55 -0300

    193. Componentes exemplo 2

--- REACT/listatarefas/src/componentes/Main.js
+++ REACT/listatarefas/src/componentes/Main.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Main extends Component {
-  // criar o estado
-  constructor(props) {
-    // sempre que temos um constructor em uma classe que herda de outra classe
-    // no caso Componente, temos que utilizar super() para chamar o contrutor da
-    // classe que estamos herdando
-    super(props);
-    // componentes podem ter propriedades (props) e devem vir no
-    // contructor e no super.
-    // Criamos o construtor e o super para poder criar a variável de estado:
-    this.state = {
-      // este estate é o estado do nosso componente.
-      // todas as chaves que estiverem aqui dentro, serão
-      // o estado do nosso componente.
-      // tudo que mudar aqui, irá refletir dentro do render
-      // inicializar o estado:
-      novaTarefa: '',
-
-    };
-    this.inputMudou = this.inputMudou.bind(this);
+  // criar o estado sem um contrutor:
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    novaTarefa: '',
   }
 
-  inputMudou(event) {
+  // transformar o método em uma arrow function:
+  // alterar o nome do método de inputMudou para handleChange
+  handleChange = (event) => {
     this.setState({
       novaTarefa: event.target.value,
     });
@@ -43,7 +30,7 @@ export default class Main extends Component {
         </h1>
 
         <form action="#">
-          <input onChange={this.inputMudou} type="text" />
+          <input onChange={this.handleChange} type="text" />
           <button type="submit">Enviar</button>
         </form>
       </div>
