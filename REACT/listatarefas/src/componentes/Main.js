0a126d919b1a9796e21d4466beff5dfa7dc50443
mandrado <mandrado@mandrado.com>
2021-10-05 10:07:54 -0300

    194. Estilizando o form com css e react-icons

--- REACT/listatarefas/src/componentes/Main.js
+++ REACT/listatarefas/src/componentes/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Main.css';
 
+// Form
+import { FaPlus } from 'react-icons/fa';
+
 export default class Main extends Component {
   // criar o estado sem um contrutor:
   // eslint-disable-next-line react/state-in-constructor
@@ -30,9 +33,11 @@ export default class Main extends Component {
           {novaTarefa}
         </h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
