8e771902353891093bf596edaa6425d27ce5ae33
mandrado <mandrado@mandrado.com>
2021-10-11 09:07:50 -0300

    210. MyRoute - Rota personalizada e privada

--- REACT/react-base/src/routes/index.js
+++ REACT/react-base/src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Login from '../pages/login';
 import Page404 from '../pages/Page404';
+import MyRoute from './MyRoute';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="*" component={Page404} />
+      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute path="*" component={Page404} />
     </Switch>
   );
 }
