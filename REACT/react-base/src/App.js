668a7bfe42c3bfb5f68c0f632c8fd909765ef953
mandrado <mandrado@mandrado.com>
2021-10-12 13:16:14 -0300

    216. Redux Persist salvando dados no storage local

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
 import history from './services/history';
-import store from './store';
+import store, { persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Header />
-        <Routes />
-        <GlobalStyles />
-        <ToastContainer autoClose={3000} className="toast-container" />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <GlobalStyles />
+          <ToastContainer autoClose={3000} className="toast-container" />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
